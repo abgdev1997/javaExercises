7ba8fe9d47ee5ef0bcefd75dbe07181542f03df3
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-01-31 17:25:04 +0100

    ejercicio 5 modificacion

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -7,24 +7,14 @@ public class Users {
     String userName;
     String email;
     String password;
-    String nationality;
-    String city;
-    List<String> studies;
-    List<String> complementaryStudies;
-    List<String> workExperience;
 
     public Users() {
     }
 
-    public Users(String userName, String email, String password, String nationality, String city, List<String> studies, List<String> complementaryStudies, List<String> workExperience) {
+    public Users(String userName, String email, String password) {
         this.userName = userName;
         this.email = email;
         this.password = password;
-        this.nationality = nationality;
-        this.city = city;
-        this.studies = studies;
-        this.complementaryStudies = complementaryStudies;
-        this.workExperience = workExperience;
     }
 
     public String getUserName() {
@@ -49,45 +39,5 @@ public class Users {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public List<String> getStudies() {
-        return studies;
-    }
-
-    public void setStudies(List<String> studies) {
-        this.studies = studies;
-    }
-
-    public List<String> getComplementaryStudies() {
-        return complementaryStudies;
-    }
-
-    public void setComplementaryStudies(List<String> complementaryStudies) {
-        this.complementaryStudies = complementaryStudies;
-    }
-
-    public List<String> getWorkExperience() {
-        return workExperience;
-    }
-
-    public void setWorkExperience(List<String> workExperience) {
-        this.workExperience = workExperience;
     }
 }
